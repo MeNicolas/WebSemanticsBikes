8a51d7d0a9284b47a05a6d6d8f8a0427eb6485ef
chloedia <chloe.daems@edu.devinci.fr>
2021-03-21 11:58:09 +0100

    adding lyon

--- index.js
+++ index.js
@@ -57,15 +57,42 @@ const context2_coord = {
     "capacity" : "capacity"
 }
 
+//for lyon api = https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=1.1.0&outputformat=GEOJSON&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171
+context_lyon = {
+	"@vocab": "http://schema.org/",
+	"@base": "http://data.org/", 
+    "number": "@id",
+    "name":"name",
+    "address": null,
+    "address2": null,
+    "commune": null,
+    "nmarrond":null,
+    "bonus":null,
+    "pole": null,
+    "lat":{"@id":"lat", "@type":"xsd:double"},
+    "lng": {"@id":"lon", "@type":"xsd:double"},
+    "bike_stands": {"@id":"capacity", "@type":"xsd:int"},
+    "status": null,
+    "available_bike_stands":{"@id":"availableDocks", "@type":"xsd:int"},
+    "available_bikes": {"@id":"availableBikes", "@type":"xsd:int"},
+    "availabilitycode":null,
+    "availability": null,
+    "banking": null,
+    "gid": null,
+    "last_update": {"@id":"lastUpdated", "@type":"xsd:dateTime"},
+    "last_update_fme": null,
+    "code_insee": null,
+    "langue":null, "etat":null, "nature": null, "titre": null, "description": null, "startdate": null, "enddate": null 
+}
 // for rennes api = https://www.star.fr/le-velo?pnfstarod_data=bike for each records.fields
-//Contains directly the coordinates (live data)
+//Contains directly the coordinates (live data) List problem
 const context3 = {
 	"@vocab": "http://schema.org/",
 	"@base": "http://data.org/",
 	"etat" : null,
 	"lastupdate":"lastUpdated",
 	"nombrevelosdisponibles": "availableBikes",
-	"nombreemplacementsactuels":null,
+	"nombreemplacementsactuels":"capacity",
 	"nom":"name",
 	"nombreemplacementsdisponibles":"availableDocks",
 	"idstation": "@id",
@@ -78,7 +105,10 @@ all_links = ["https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropo
 			"https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json",
 			"https://saint-etienne-gbfs.klervi.net/gbfs/en/station_status.json",
 			"https://saint-etienne-gbfs.klervi.net/gbfs/en/station_information.json",
-			"https://www.star.fr/le-velo?pnfstarod_data=bike"]
+			"https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=1.1.0&outputformat=GEOJSON&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171"
+
+			//"https://www.star.fr/le-velo?pnfstarod_data=bike"
+		]
 			
 // jsonld_request('https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json', async function(err, res, data) {
 // 	let doc = data.data.stations
@@ -95,14 +125,15 @@ all_links = ["https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropo
 // 	}});*/
 // });
 
-jsonld_request('https://www.star.fr/le-velo?pnfstarod_data=bike', async function(err, res, data) {
+jsonld_request('https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=1.1.0&outputformat=GEOJSON&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171', async function(err, res, data) {
 	data = JSON.parse(res.body)
-	let doc = data.records.map(r => r.fields)
-	doc.forEach(s => s['@context'] = context_coord)
-	// jsonld.compact(doc, context)
-	// .then(compacted => 
-	// 	console.log(JSON.stringify(compacted, null, 2))
-	// );
+	data = data.features
+	let doc = data.map(r => r.properties)
+	doc.forEach(s => s['@context'] = context_lyon)
+	 /*jsonld.compact(doc, context_lyon)
+	 .then(compacted => 
+	 	console.log(JSON.stringify(compacted, null, 2))
+	 );*/
 	
 	const rdf = await jsonld.toRDF(doc, {format: 'application/n-quads'});
 	fs.writeFile('output3.ttl', rdf, function(err) {
